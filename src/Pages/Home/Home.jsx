4d1fdde0b76f0bdb1ac02d590586f44669ee3699
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-10-22 21:45:24 +0200

    last design fix home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -19,50 +19,68 @@ const Home = () => {
       <div className="flex flex-col items-center justify-between lg:flex-row">
         <div className="relative lg:w-1/2">
           <Lottie animationData={bookingAnimation} />
+          <button
+            type="button"
+            className="px-8 py-3 font-semibold border-0 bg-sky-400 flex justify-center items-center  rounded border-gray-100 text-slate-900"
+          >
+            Book Now
+            <HiArrowLongRight className="ml-2" />
+          </button>
         </div>
-        <div className="mb-10 lg:max-w-lg lg:pr-5 lg:mb-0">
+        <div className="mb-10 lg:max-w-lg  lg:mb-0 ">
           {/* ** Swipper js */}
           {/* cards destinations */}
-          <div className="w-full">
-            <Swiper
-              slidesPerView={3}
-              spaceBetween={30}
-              pagination={{
-                clickable: true,
-              }}
-              modules={[Pagination]}
-              className="mySwiper"
-            >
-              {destinations.map((destination) => (
-                <>
-                  <SwiperSlide style={{ width: "100%" }}>
-                    <img
-                      className="ring-4 rounded  ring-yellow-500  "
-                      style={{ width: "100%", height: "300px" }}
-                      src={destination.picture}
-                      alt=""
-                    />
-                    <div className="text-center">
-                      <small className="text-blue-700 mt-2 text-center flex items-center justify-center">
-                        <span>
-                          {" "}
-                          <MdPlace />
-                        </span>
-                        <p>{destination.place}</p>
-                      </small>
-                    </div>
-                  </SwiperSlide>
-                </>
-              ))}
-            </Swiper>
-            <button
-              type="button"
-              className="px-8 py-3 font-semibold border-0 bg-sky-400 flex justify-center items-center  rounded border-gray-100 text-slate-900"
-            >
-              Book Now
-              <HiArrowLongRight className="ml-2" />
-            </button>
-          </div>
+
+          <Swiper
+            slidesPerView={2.5}
+            spaceBetween={10}
+            autoplay={true}
+            pagination={{
+              clickable: true,
+            }}
+            breakpoints={{
+              "@0.00": {
+                slidesPerView: 1,
+                spaceBetween: 10,
+              },
+              "@0.75": {
+                slidesPerView: 2,
+                spaceBetween: 20,
+              },
+              "@1.00": {
+                slidesPerView: 3,
+                spaceBetween: 40,
+              },
+              "@1.50": {
+                slidesPerView: 4,
+                spaceBetween: 50,
+              },
+            }}
+            modules={[Pagination]}
+            className="mySwiper"
+          >
+            {destinations.map((destination) => (
+              <>
+                <SwiperSlide className="w-50">
+                  <img
+                    className="ring-4 rounded-2xl  ring-yellow-500  "
+                    style={{ height: "300px", width: "100%" }}
+                    src={destination.picture}
+                    alt=""
+                  />
+                  <div className="text-center">
+                    <small className="text-blue-700 mt-2 text-center flex items-center justify-center">
+                      <span>
+                        {" "}
+                        <MdPlace />
+                      </span>
+                      <p>{destination.place}</p>
+                    </small>
+                  </div>
+                </SwiperSlide>
+              </>
+            ))}
+          </Swiper>
         </div>
       </div>
     </div>
